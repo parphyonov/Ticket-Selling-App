569af4676902fcdcb017e73ece6732ada0667bd3
Alexei Parphyonov <alexei.s.parphyonov@gmail.com>
2018-05-26 17:15:55 +0300

    Commit #02
    1. Errors are reported in a user-friendly manner.
    2. Users are able to confirm their order before purchase.
    3. The total sum is told to users beforehand.
    4. Users are able to exit the loop if they enter a non-int value when asked about the amount of tickets they would like to buy.

--- ticket-app.py
+++ ticket-app.py
@@ -20,22 +20,63 @@ def check_availability():
         return True
 
 def check_number(tickets_remaining):
-    print('')
     if tickets_remaining == 1:
-        print('There are currently only one ticket on sale!')
+        return 'ticket'
     else:
-        print('There are currently {} tickets on sale!'.format(tickets_remaining))
+        return 'tickets'
+
+def apply_ticket_request(tickets_remaining, TICKET_PRICE, user_name):
+    while True:
+        print('')
+        try:
+            request = int(input('How many tickets you would like to buy? '))
+        except ValueError:
+            print('')
+            print('It looks like you provided something non-numerical. Please, provide a number under {} and above 0.'.format(tickets_remaining))
+            should_exit = input('Or enter EXIT to leave the app: ')
+            if should_exit.lower() == 'exit':
+                break
+            else:
+                continue
+        else:
+            if request > tickets_remaining:
+                print('')
+                print('We are unable to accommodate your request. We are under the amount of tickets you have requested. We currently have {} {}.'.format(tickets_remaining, check_number(tickets_remaining)))
+            elif request == 0:
+                print('')
+                print('We would be happy to be able to sell nothing with our margin, but unfortunately you need to inquire at least one ticket.')
+            elif request < 0:
+                print('')
+                print('There cannot be a negative amount of tickets. Maybe, in parallel universe... But surely, next time.')
+            else:
+                print('')
+                print('You are about to purchase {} {}.'.format(request, check_number(request)))
+                print('It is going to cost you {} usd.'.format(request * TICKET_PRICE))
+                print('')
+                confirmation_of_purchase = input('Would you like to confirm? (Y/n) ')
+                if confirmation_of_purchase.lower() != 'y':
+                    continue
+                else:
+                    print('')
+                    print('Thank you very much for your purchase, {}. See you at the performance!'.format(user_name))
+                    return tickets_remaining - request
 
 def ticket_app(tickets_remaining, TICKET_PRICE):
 
     if check_availability():
         user_name = greet_user()
         print('')
-        print('Welcome to the Tickets App, {}!'.format(user_name))
-        check_number(tickets_remaining)
+        print('Welcome to Tickets App, {}!'.format(user_name))
+        print('')
+        if tickets_remaining == 1:
+            print('There are currently only one ticket on sale!')
+        else:
+            print('There are currently {} tickets on sale!'.format(tickets_remaining))
+
+        tickets_remaining = apply_ticket_request(tickets_remaining, TICKET_PRICE, user_name)
 
     print('')
-    print('Thank you for using Tickets App! We hope to see you again!')
+    print('Thank you for using Tickets App, {}! We hope to see you again!'.format(user_name))
     print('')
 
 ticket_app(tickets_remaining, TICKET_PRICE)
